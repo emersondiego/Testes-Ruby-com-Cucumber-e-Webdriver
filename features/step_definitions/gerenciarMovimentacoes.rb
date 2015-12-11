4deda0b26f77c98a07a585297741723462075a87
Emerson Pereira <edap.diego@gmail.com>
2015-12-10 23:37:16 -0200

    primeiro teste rodando

--- features/step_definitions/gerenciarMovimentacoes.rb
+++ features/step_definitions/gerenciarMovimentacoes.rb
@@ -4,12 +4,14 @@ Dado(/^que eu esteja autenticado como administrador$/) do
   @navegador = Selenium::WebDriver.for :firefox
   @navegador.manage.window.maximize
   @navegador.manage.timeouts.implicit_wait = 5
-  @navegador.get 'http://localhost:4567/quickloja/'
-
+  @navegador.get 'http://www.testadores.com/'
+  @navegador.find_element(:id, 'modlgn_username').send_keys("testes")
+  @navegador.find_element(:id, 'modlgn_passwd').send_keys("inicial1234")
+  @navegador.find_element(:css, '.input input.button').click
 end
 
 # Dado(/^que eu tenha acessado o menu "([^"]*)"$/) do |arg1|
-#   pending # Write code here that turns the phrase above into concrete actions
+#   pending # Write code here that turns the phrase above into concrete actions.input input.button
 # end
 
 # Dado(/^clicado em "Nova movimentação” para inserir uma movimentação$/) do
---
+++ features/support/env.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+require 'cucumber/formatter/unicode'
+require 'rspec/expectations'
+require 'selenium-webdriver'
